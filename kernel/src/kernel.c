a8b0caa25ef6d7a8b796c44e6906247bf3b8ae90
jwt2706 <jwt2home@gmail.com>
2024-08-25 23:46:04 -0400

    font fixes

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -65,12 +65,9 @@ void _start(void) {
 
     // init the terminal
     struct terminal term;
-    terminal_init(&term, framebuffer, COLOR_WHITE, 2);
+    terminal_init(&term, framebuffer, COLOR_GREEN, 2);
 
-    terminal_write(&term, "BaboscOS booted up successfully!BaboscOS booted up successfully!BaboscOS booted up successfully!");
-    terminal_edit(&term, COLOR_RED, 3);
-    terminal_write(&term, "red color and scalesadasdasdsads 3");
-
+    terminal_write(&term, "BaboscOS booted up successfully!\nuser@babosc:~$ _");
     // We're done, just hang...
     hcf();
 }
