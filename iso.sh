5c389b0975b9cc670e4899ae9ee1630ba3fb08ad
jwt2706 <jwt2home@gmail.com>
2024-09-17 22:42:11 -0400

    rename os

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/BaboscOS.kernel isodir/boot/BaboscOS.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "BaboscOS" {
+	multiboot /boot/BaboscOS.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o BaboscOS.iso isodir
